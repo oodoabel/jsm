62bcccb98b25b15e63ec9e9ed75c5d37a05d361b
oodoabel <get2abeloodo@gmail.com>
2025-02-01 01:01:56 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 
 const Card = ( { title } ) => {
+
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
-    <h2>      {title}    </h2>
+    <div style={{
+      border: '1px solid #4b5262',
+      padding: '20px',
+      margin: '10px',
+      backgroundColor:' #4b5262',
+      borderRadius: '10px',
+      paddingTop:'50px'
+    }}>
+      <h2>      {title}   <br /><button onClick={() => setHasLiked(!hasLiked)}>{hasLiked ? '🤍': '💔'}</button>
+         </h2>
+    </div>
   )
 }
 
 
 function App() {
+
 
   return (
     <>
