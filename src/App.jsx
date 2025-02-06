bdefd68b935b23ea53a83b4f1ffba705e34990e0
oodoabel <get2abeloodo@gmail.com>
2025-02-06 17:01:06 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './assets/components/Search'
 
+const API_BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=6b4a9cca';
 
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
 
   const [ searchTerm, setSearchTerm ] = useState('');
 
+  const [ errorMessage, setErrorMessage ] = useState('')
+
+  const fetchMovies = async () => {
+    try{
+
+      const endpoint = `${API_BASE_URL}/discover/movies?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      throw new Error('Failed to fetch movies');
+
+    } catch(error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later');
+    } 
+  }
+
+  useEffect( () => {
+    fetchMovies()
+  })
   return (
     <main>
     <div className='pattern'/>
